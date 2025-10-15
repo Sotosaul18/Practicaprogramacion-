c24533fe1748018a8709ca9a02071ba352ec30e5
Sotosaul18 <a385853@uach.mx>
2025-10-15 08:35:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ radio = st.slider("Selecciona el radio", 0.0, 10.0, 5.0)
 area = math.pi * radio **2
 #Mostrar resultado
 st.write(f"El área del círculo con radio (radio) es:{area:.2f}")
+
+figura = st.selectbox("Selecciona una figura:", "Circulo", "Triangulo", "Rectángulo", "Cuadrado")
+area = None
+perimetro = None
+if figura == "Circulo":
+  st.subheader("Circulo")
+  r=st.number_input("Ingresa el radio(r):", min_value=0.0, step=0.1)
+  if r > 0:
+    area = math.pi * r**2
+    perimetro = 2 * math.pi * r
